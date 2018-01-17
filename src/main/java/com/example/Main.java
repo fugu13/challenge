b4d1646d35053999141dfa3ae231a2ee82f5dea4
Russell Duhon <fugu13@gmail.com>
2018-01-17 15:24:35 -0800

    okay, this change gets the war loading and the API working and index.html loading... but not app.js, at original path or at /static/ or at /public/

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,12 +2,19 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * For starting the application without deploying to an application server
  */
 @SpringBootApplication
-public class Main {
+public class Main extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Main.class);
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Main.class, args);
